ad3408889d3ee605a75a3aec24bb50c866aae7d5
Angie-540 <janiceangella21@gmail.com>
2023-10-12 00:13:16 +0300

    navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,17 +27,25 @@ var swiperCategories = new Swiper('.categories__container', {
     },
 
     breakpoints: {
-        640: {
-            slidesPerView:2,
-            spaceBetween:20,
+        350: {
+            slidesPerView: 2,
+            spaceBetween: 24,
         },
         768: {
-            slidesPerView:4,
-            spaceBetween:40,
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
         },
         1400: {
-            slidesPerView:6,
-            spaceBetween:24,
+            slidesPerView: 6,
+            spaceBetween: 24,
         },
     }
 });
@@ -53,17 +61,17 @@ var swiperProducts = new Swiper('.new__container', {
     },
 
     breakpoints: {
-        640: {
-            slidesPerView:2,
-            spaceBetween:20,
-        },
         768: {
-            slidesPerView:4,
-            spaceBetween:40,
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 24,
         },
         1400: {
-            slidesPerView:4,
-            spaceBetween:24,
+            slidesPerView: 4,
+            spaceBetween: 24,
         },
     }
 });
